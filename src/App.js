e74f4cf31c3a30c3a0923bbdef4c62875ef7c803
SwarnaKishore <swarnakishoree@gmail.com>
2020-12-30 14:47:29 -0600

    updated to use event source for getting wiki media stream for every new page create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+      data: []
+  };
+
+  componentDidMount() {
+      const eventSource = new EventSource(`https://stream.wikimedia.org/v2/stream/page-create`);
+      eventSource.onmessage = e => {
+      const result = JSON.parse(e.data);
+      this.setState({
+                    data: [result, ...this.state.data]
+                });
+    };
+  }
+
+  render() {
+      const { data } = this.state;
+      const result = data.map((entry, index) => {
+          return <li key={index}><a href={entry.meta.uri} target="_blank">{entry.page_title}</a></li>;
+      });
+      return <div className="container"><ul>{result}</ul></div>;
+  }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+//}
 
 export default App;
